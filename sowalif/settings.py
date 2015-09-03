f607f1e8081df998bedb649afc99c4a1ca7f4ac7
essaibrahim <essa.ibrahim@gmail.com>
2015-09-03 14:50:05 +0300

    Database (mysql) settings

--- sowalif/settings.py
+++ sowalif/settings.py
@@ -76,11 +76,16 @@ WSGI_APPLICATION = 'sowalif.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sowalif',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'PORT': ''
     }
 }
+
 
 
 # Internationalization
