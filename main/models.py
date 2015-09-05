23bd7fbf25e11e6c47ba73b87353b5fcc748b39f
sentient64 <m.alsharekh94@gmail.com>
2015-09-05 15:44:34 +0300

    models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, required=True)
+    tag = models.ManyToManyField('main.Tag')
+
+    def __unicode__(self):
+        return self.user.username
