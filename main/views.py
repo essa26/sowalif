baaa87fb44936619f53e7dd59ea9630ee92342c8
essaibrahim <essa.ibrahim@gmail.com>
2015-09-05 14:41:36 +0300

    haha lololol

--- main/views.py
+++ main/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from main.models import Post
 
-def signup():
-    return "hi"
+
+
+
+def post_create(request):
+
+
+
+
+
+def post_detail_view(request, pk):
+
+    context = {}
+
+    posts = Post.objects.get(pk=pk)
+
+    context['posts'] = posts
+
+
+    return render_
